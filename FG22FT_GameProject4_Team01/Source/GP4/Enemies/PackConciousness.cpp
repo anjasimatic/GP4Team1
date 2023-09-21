bdc955b50853cf8ff1d2c5a75c5ee8ba89bba403
SokySergeant <sokysergent147@gmail.com>
2023-09-21 13:45:57 +0200

    Added pack conciousness

---
+++ FG22FT_GameProject4_Team01/Source/GP4/Enemies/PackConciousness.cpp
@@ -0,0 +1,78 @@
+﻿#include "PackConciousness.h"
+#include "EnemyAIController.h"
+#include "Grunt.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "GP4/Components/HealthComponent.h"
+#include "Kismet/GameplayStatics.h"
+
+APackConciousness::APackConciousness()
+{
+	PrimaryActorTick.bCanEverTick = false;
+}
+
+void APackConciousness::BeginPlay()
+{
+	Super::BeginPlay();
+
+	//Get all grunts
+	UGameplayStatics::GetAllActorsOfClass(GetWorld(), AGrunt::StaticClass(), AllGrunts);
+
+	//Remove any non anger grunts
+	for(int i = 0; i < AllGrunts.Num(); i++)
+	{
+		if(Cast<AGrunt>(AllGrunts[i])->MyEmotion != EEmotionType::Anger) AllGrunts.RemoveAt(i);
+	}
+
+	//Bind delegate to on enemy aggro
+	for (TObjectPtr<AActor> Grunt : AllGrunts)
+	{
+		TObjectPtr<AEnemyAIController> Controller = Cast<AEnemyAIController>(Cast<APawn>(Grunt)->GetController());
+		Controller->OnAggro.AddDynamic(this, &APackConciousness::OnEnemyAggro);
+	}
+}
+
+void APackConciousness::OnEnemyAggro(APawn* Enemy)
+{
+	AggroControllers.AddUnique(Cast<AEnemyAIController>(Enemy->GetController()));
+	ReEvaluateWhoseTurnItIs();
+}
+
+void APackConciousness::ReEvaluateWhoseTurnItIs()
+{
+	if(AggroControllers.IsEmpty()) return;
+
+	//Stop current grunt whose turn it is 
+	for(int i = 0; i < AggroControllers.Num(); i++)
+	{
+		AggroControllers[i]->GetBlackboardComponent()->SetValueAsBool(TEXT("IsMyTurn"), false);
+	}
+
+	//Find out which enemy is best fit to attack next
+	int BestIndex = 0;
+	float ClosestDist = 100000.f;
+	for(int i = 0; i < AggroControllers.Num(); i++)
+	{
+		float Dist = FVector::Distance(AggroControllers[i]->GetPawn()->GetActorLocation(), UGameplayStatics::GetPlayerPawn(GetWorld(), 0)->GetActorLocation());
+		if(Dist < ClosestDist)
+		{
+			ClosestDist = Dist;
+			BestIndex = i;
+		}
+	}
+
+	//Let chosen enemy attack
+	AggroControllers[BestIndex]->GetBlackboardComponent()->SetValueAsBool(TEXT("IsMyTurn"), true);
+	GruntWhoseTurnItIs = AggroControllers[BestIndex]->GetPawn();
+	Cast<UHealthComponent>(GruntWhoseTurnItIs->GetComponentByClass(UHealthComponent::StaticClass()))->OnDeath.AddDynamic(this, &APackConciousness::OnEnemyDeath);
+}
+
+void APackConciousness::OnEnemyDeath()
+{
+	//Cleanup
+	for(int i = 0; i < AggroControllers.Num(); i++)
+	{
+		if(!AggroControllers[i]->GetPawn()) AggroControllers.RemoveAt(i);
+	}
+	
+	ReEvaluateWhoseTurnItIs();
+}
---
+++ FG22FT_GameProject4_Team01/Source/GP4/Enemies/PackConciousness.h
@@ -0,0 +1,34 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "PackConciousness.generated.h"
+
+class AEnemyAIController;
+
+UCLASS()
+class GP4_API APackConciousness : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	APackConciousness();
+
+protected:
+	virtual void BeginPlay() override;
+
+	UPROPERTY()
+	TArray<TObjectPtr<AActor>> AllGrunts;
+	UPROPERTY(VisibleAnywhere)
+	TArray<TObjectPtr<AEnemyAIController>> AggroControllers;
+	UPROPERTY()
+	TObjectPtr<APawn> GruntWhoseTurnItIs;
+
+	UFUNCTION()
+	void OnEnemyAggro(APawn* Enemy);
+	UFUNCTION()
+	void OnEnemyDeath();
+
+	UFUNCTION()
+	void ReEvaluateWhoseTurnItIs();
+};
