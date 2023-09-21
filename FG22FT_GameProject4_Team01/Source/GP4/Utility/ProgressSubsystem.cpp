db19b718a2e67d0254ca2f984ae69befc82327b7
Junder-2 School Computer <55179209+Junder-2@users.noreply.github.com>
2023-09-21 12:23:35 +0200

    Added progress saving

---
+++ FG22FT_GameProject4_Team01/Source/GP4/Utility/ProgressSubsystem.cpp
@@ -0,0 +1,22 @@
+﻿#include "ProgressSubsystem.h"
+
+void UProgressSubsystem::Initialize(FSubsystemCollectionBase& Collection)
+{
+	LifeStageProgress = 0;
+	Super::Initialize(Collection);
+}
+
+void UProgressSubsystem::Deinitialize()
+{
+	Super::Deinitialize();
+}
+
+void UProgressSubsystem::SetLifeStageProgress(const int NewStage)
+{
+	LifeStageProgress = NewStage;
+}
+
+int UProgressSubsystem::GetLifeStageProgress() const
+{
+	return LifeStageProgress;
+}
